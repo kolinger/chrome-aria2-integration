eaf7c67ce8344ea65eb910dbd367791c28a2b0ff
Tomáš Kolinger <tomas@kolinger.name>
2019-07-23 07:55:39 +0200

    fixed settings

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-var settings = new Store('settings', {
-	"rpcpath": "http://localhost:6800/jsonrpc",
-	"rpcuser": "",
-	"rpctoken": "",
-	"filesizesetting": "500M",
-	"whitelisttype": "",
-	"whitelistsite": "",
-	"blacklistsite": "",
-	"captureCheckbox": true,
-	"sizecaptureCheckbox": true
-});
-
 var ARIA2 = (function () {
 	"use strict";
 
---
+++ shared.js
@@ -0,0 +1,11 @@
+var settings = new Store('settings', {
+	"rpcpath": "http://localhost:6800/jsonrpc",
+	"rpcuser": "",
+	"rpctoken": "",
+	"filesizesetting": "500M",
+	"whitelisttype": "",
+	"whitelistsite": "",
+	"blacklistsite": "",
+	"captureCheckbox": true,
+	"sizecaptureCheckbox": true
+});
